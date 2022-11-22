c68ebe1263f24397e47cd96a8e4f50dd4e1b3e8a
RexLeung0919 <leunglapmun@gmail.com>
2022-11-23 04:11:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -128,12 +128,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     // Display an error message if username doesn't exist
                     $username_err = "No account found with that username.";
                 } 
-                else{
+                }else{
                     // Display an error message if token wrong
                     $val_text_err_err = "Verification Failed.";
                 }
-            }
-            } else{}
+            } 
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
